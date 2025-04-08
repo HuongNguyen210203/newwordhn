910c73de5823d6ba512c6b628653df3c4c293a84
LaloTheDrug <kiet0966923001@gmail.com>
2025-04-08 09:28:52 +0700

    create

---
+++ src/app/components/card-room/card-room.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-room',
+  imports: [],
+  templateUrl: './card-room.component.html',
+  styleUrl: './card-room.component.css'
+})
+export class CardRoomComponent {
+
+}
